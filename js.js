804e4c9875b7433dd4a2d6f9d28ebeeb4df2e44a
dpgalloway <devin.galloway@erg.com>
2017-02-01 22:15:10 -0700

    another test

--- js.js
+++ js.js
@@ -1,19 +1,18 @@
 $(document).ready(function () {
     $('.hideOnLoad').hide();
-	console.log("TEST1");
-});
-$('nav a').click(function () {
-	console.log("TEST2");
-    var active = $('.section:visible');
-    var newActive = $(this).attr('href');
-    if ($(active).attr('id') != $(newActive).attr('id')) {
-        $(active).hide('slide', {
-            direction: 'up',
-            duration: 500
-        });
-        $(newActive).delay(500).show('slide', {
-            direction: 'up',
-            duration: 500
-        });
-    }
+	$('nav a').click(function () {
+		console.log("TEST2");
+		var active = $('.section:visible');
+		var newActive = $(this).attr('href');
+		if ($(active).attr('id') != $(newActive).attr('id')) {
+			$(active).hide('slide', {
+				direction: 'up',
+				duration: 500
+			});
+			$(newActive).delay(500).show('slide', {
+				direction: 'up',
+				duration: 500
+			});
+		}
+	});
 });
